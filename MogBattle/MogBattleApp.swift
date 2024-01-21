e2f99f0313a3abbfa626fc1c1dcaef605291c339
Harry Morgan <psyhm6@nottingham.ac.uk>
2024-01-21 20:32:07 +0000

    Initial Commit

---
+++ MogBattle/MogBattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MogBattleApp.swift
+//  MogBattle
+//
+//  Created by Harry Morgan on 21/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MogBattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
